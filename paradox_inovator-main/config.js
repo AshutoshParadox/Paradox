a86a1eb4af045bb5a29c83e7a5812b2cd2dc80e7
Alertashu <151422883+Alertashu@users.noreply.github.com>
2025-01-13 00:37:42 +0530

    files added

---
+++ paradox_inovator-main/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    base: "/paradox_innovator",
+    publicPath: "/paradox_innovator/",
+    outputPath: "dist",
+    deploymentConfig: {
+      repo: "paradox_inovator",
+      branch: "gh-pages",
+    },
+};
